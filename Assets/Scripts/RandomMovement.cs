5fd1902e6445978ce576cc0b3433fadf1165e08e
ashura1234 <b97501063@gmail.com>
2020-02-06 01:07:45 -0800

    Initial commit

---
+++ Assets/Scripts/RandomMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public GameObject parent;
+    float xMax;
+
+    void Update()
+    {
+
+    }
+}
