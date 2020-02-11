6f798960d1b97efb2fb435da32c1b26b07af1739
ashura1234 <b97501063@gmail.com>
2020-02-10 22:14:30 -0800

    021020
    Added Randommovement

---
+++ Assets/Scripts/DDOL.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DDOL : MonoBehaviour
+{
+    public void Awake(){
+        DontDestroyOnLoad(gameObject);
+        print("_preload scene loaded.");
+        if (LoadingSceneIntegration.otherScene > 0)
+        {
+            Debug.Log("Returning again to the scene: " + LoadingSceneIntegration.otherScene);
+            SceneManager.LoadScene(LoadingSceneIntegration.otherScene);
+        }
+    }
+}
--- Assets/Scripts/RandomMovement.cs
+++ Assets/Scripts/RandomMovement.cs
@@ -5,11 +5,34 @@ using UnityEngine;
 public class RandomMovement : MonoBehaviour
 {
     public float moveSpeed;
-    public GameObject parent;
-    float xMax;
+    protected Vector2 bound;
+    protected float timer;
 
+
+    void Start()
+	{
+        Transform parent = this.transform.parent;
+        bound = parent.GetComponent<BoxCollider2D>().size;
+        timer = 0f;
+	}
     void Update()
     {
-
+        timer += Time.deltaTime;
+        if(timer > 3)
+		{
+            Vector3 target = Move();
+            print("Move to " + target);
+            this.transform.localPosition = Vector3.MoveTowards(this.transform.localPosition, target, moveSpeed * Time.deltaTime);
+            print(this.transform.position);
+            timer = 0;
+        }
     }
+
+    public Vector3 Move()
+	{
+        Vector3 boundV3 = bound;
+        Vector3 target = this.transform.parent.position;
+        target += boundV3 * Random.Range(-1f, 1f) / 2;
+        return target;
+	}
 }
